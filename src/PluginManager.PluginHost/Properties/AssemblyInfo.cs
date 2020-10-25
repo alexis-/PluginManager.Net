61dfef0157c999ca44455f11b39c4b0e162c07e8
Alexis <alexis@incogito.org>
2020-10-25 16:22:24 +0100

    - Updated: Packages

--- src/PluginManager.PluginHost/Properties/AssemblyInfo.cs
+++ src/PluginManager.PluginHost/Properties/AssemblyInfo.cs
@@ -81,7 +81,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.1.49")]
-[assembly: AssemblyFileVersion("0.2.1.49")]
+[assembly: AssemblyVersion("0.2.1.50")]
+[assembly: AssemblyFileVersion("0.2.1.50")]
 
 [assembly: AssemblyInformationalVersion("0.2.1")]
